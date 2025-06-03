e328c2595f822352521e3e5c03deb1aa07210199
zh1peng <frueddy@gmail.com>
2025-06-03 14:46:11 +0800

    tweaks

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from psyflow import BlockUnit,StimBank, StimUnit,SubInfo,TaskSettings,TriggerSender
+from psyflow import BlockUnit,StimBank, StimUnit,SubInfo,TaskSettings, TriggerSender
 from psyflow import load_config,count_down, initialize_exp
 import pandas as pd
 from psychopy import core
@@ -21,23 +21,27 @@ settings.add_subinfo(subject_data)
 # 4. setup triggers
 settings.triggers = cfg['trigger_config']
 ser = serial.serial_for_url("loop://", baudrate=115200, timeout=1)
+# ser = serial.Serial("COM3", baudrate=115200, timeout=1)
 trigger_sender = TriggerSender(
     trigger_func=lambda code: ser.write([1, 225, 1, 0, (code)]),
-    post_delay=0,
+    post_delay=0.001,
     on_trigger_start=lambda: ser.open() if not ser.is_open else None,
     on_trigger_end=lambda: ser.close()
 )
 
 # 5. Set up window & input
 win, kb = initialize_exp(settings)
+
 # 6. Setup stimulus bank
 stim_bank = StimBank(win,cfg['stim_config']).\
             convert_to_voice(['general_instruction', 'EC_instruction', 'EO_instruction', 'good_bye'])\
             .preload_all()
 # stim_bank.preview_all() 
 
+settings.save_to_json() # save all settings to json file
 
-StimUnit('block', win, kb)\
+trigger_sender.send(settings.triggers.get("exp_onset"))
+StimUnit('instruction',win,kb)\
     .add_stim(stim_bank.get('general_instruction'))\
     .add_stim(stim_bank.get('general_instruction_voice'))\
     .wait_and_continue()
@@ -55,14 +59,12 @@ block = BlockUnit(
     
     
 
-StimUnit('block', win, kb)\
+StimUnit('goodbye',win,kb)\
     .add_stim(stim_bank.get('good_bye'))\
     .add_stim(stim_bank.get('good_bye_voice'))\
     .wait_and_continue(terminate=True)
-    
+trigger_sender.send(settings.triggers.get("exp_end"))
 
-df = pd.DataFrame(block.get_dict())
-df.to_csv(settings.res_file, index=False)
 core.quit()
 
 
